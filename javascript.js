8c5df906cf02771376b36772d49db0ad99c95d04
Se1wan <MichiBillerbeck@gmail.com>
2021-03-26 21:22:58 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 <script>
-alert("hello world");
+const Http = new XMLHttpRequest();
+const url='https://webhook.site/595219f2-fa1b-4278-b915-66d4113ce8d8';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
 </script>
