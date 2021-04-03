c2193d957617a9db4a6f828c578e3e23c15ec10b
Se1wan <MichiBillerbeck@gmail.com>
2021-04-03 18:34:46 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 <script>
 const Http = new XMLHttpRequest();
-const url='https://webhook.site/595219f2-fa1b-4278-b915-66d4113ce8d8';
+const url='https://webhook.site/595219f2-fa1b-4278-b915-66d4113ce8d8/test1234';
 Http.open("GET", url);
 Http.send();
 
